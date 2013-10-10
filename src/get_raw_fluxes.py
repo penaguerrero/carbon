7d27c2949221864c28018494e01d91967ebb4626
Maria Peña-Guerrero <pena@stsci.edu>
2013-10-10 18:06:39 -0400

    improved code

--- src/get_raw_fluxes.py
+++ src/get_raw_fluxes.py
@@ -10,30 +10,40 @@ It writes a text file containing the line information it found for that waveleng
 
 ##################################################################################################################################
 
+''' OBJECTS OF THE SAMPLE '''
+
 # name of the object
 objects_list =['arp252', 'iiizw107', 'iras08208', 'iras08339', 'mrk5', 'mrk960', 
                'mrk1087', 'mrk1199', 'ngc1741', 'pox4', 'sbs0218', 'sbs0948', 
                'sbs0926', 'sbs1054', 'sbs1319', 'tol9', 'tol1457']
-# choose a number from objects_list:
-#              arp252 =0, iiizw107 =1, iras08208 =2, iras08339 =3, mrk5 =4, mrk960 =5, 
-#              mrk1087 =6, mrk1199 =7, ngc1741 =8, pox4 =9, sbs0218 =10,  sbs0948 =11, 
-#              sbs0926 =12, sbs1054 =13, sbs1319 =14, tol9 =15, tol1457 =16
-object_name = objects_list[0]
-
-# redshift of the object: same number as in object_list
+# corresponding redshifts
 z_list = [0.032989, 0.01972, 0.04678, 0.19113, 0.002695, 0.021371,
           0.02877, 0.013454, 0.01348, 0.01997, 0.058841, 0.0464,
           0.01394, 0.002010, 0.0077, 0.011625, 0.01763]
-# choose a z from z_list (same numbers as in object_list)
-z = z_list[0]
 
-# use all 3 files for NUV, optical, and NIR?
-whole_spectrum = True
-# if False, type which ones to use: nuv=0, opt=1, nir=2
+##################################################################################################################################
+
+# Choose parameters to run script
+
+# 1) Select a number from objects_list, i = :
+#              arp252 =0, iiizw107 =1, iras08208 =2, iras08339 =3, mrk5 =4, mrk960 =5, 
+#              mrk1087 =6, mrk1199 =7, ngc1741 =8, pox4 =9, sbs0218 =10,  sbs0948 =11, 
+#              sbs0926 =12, sbs1054 =13, sbs1319 =14, tol9 =15, tol1457 =16
+object_number = 0
+object_name = objects_list[object_number]
+z = z_list[object_number]
+
+# 2) use all 3 files for NUV, optical, and NIR?
+whole_spectrum = False
+# 2.a) if False, type which ones to use: nuv=0, opt=1, nir=2
 specs = [1]
 
-# write the text file with the line net fluxes and equivalent widths?
-text_table = True
+# 3) Do you want to use Vacuum wavelengths?
+vacuum = False
+
+# 4) write the text file with the line net fluxes and equivalent widths?
+text_table = False
+
 
 ##################################################################################################################################
 
@@ -50,16 +60,17 @@ results4object_path = os.path.join(full_results_path, object_name)
 nuv = object_name+"_selectedspecs_nuv.txt"
 opt = object_name+"_selectedspecs_opt.txt"
 nir = object_name+"_selectedspecs_nir.txt"
-text_file_list = [nuv, opt, nir]
+full_file_list = [nuv, opt, nir]
 
 if whole_spectrum == False:
     new_text_file_list = []
     for item in specs:
-        tf = text_file_list[item]
+        tf = full_file_list[item]
         new_text_file_list.append(tf)
     text_file_list = new_text_file_list
 elif whole_spectrum == True:
     specs = [0, 1, 2]
+    text_file_list = full_file_list
     
 # List of the data contained in each file
 data = []
@@ -74,14 +85,14 @@ for i in range(len(text_file_list)):
 # Terminations used for the lines text files
 spectrum_region = ["_nuv", "_opt", "_nir"]
 for d, s in zip(data, specs):
-    print 'Working with:  %s' % text_file_list[s]
+    print 'Working with:  %s' % full_file_list[s]
     # Correct spectra for redshift and calculate continuum with a polynomial of nth order
     object_spectra, fitted_continuum = spectrum.fit_continuum(d, z, nth=5, thresold_fraction=1, window_wdith=150, normalize=False)
     
     # Obtain the lines net fluxes and EWs
     new_file_name = object_name+spectrum_region[s]+"_lineinfo.txt"
     lineinfo_text_file = os.path.join(results4object_path, new_file_name)
-    object_lines_info = spectrum.find_lines_info(object_spectra, fitted_continuum, lineinfo_text_file, text_table=text_table, vacuum=True)
+    object_lines_info = spectrum.find_lines_info(object_spectra, fitted_continuum, lineinfo_text_file, text_table=text_table, vacuum=vacuum)
     print ''
     
 print 'Code finished!'
