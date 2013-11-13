10f25dfb834ec591c99ffea316f44d657eba0730
Maria Peña-Guerrero <penaguerrero.m@gmail.com>
2013-11-13 12:14:56 -0500

    important error changes

--- src/get_raw_fluxes.py
+++ src/get_raw_fluxes.py
@@ -26,12 +26,12 @@ z_list = [0.032989, 0.01972, 0.04678, 0.19113, 0.002695, 0.021371, 0.02877, 0.01
 # 1) Select a number from objects_list, i = :
 #       arp252 = 0,  iiizw107 = 1,  iras08208 = 2,  iras08339 = 3,  mrk5 = 4,  mrk960 = 5, mrk1087 = 6,  mrk1199 = 7,  ngc1741 = 8,  
 #       pox4 =9,  sbs0218 = 10,  sbs0948 = 11, sbs0926 = 12,  sbs1054 = 13,  sbs1319 = 14,  tol9 =15,  tol1457 = 16
-object_number = 14
+object_number = 9
 object_name = objects_list[object_number]
 z = z_list[object_number]
 
 # 2) use all 3 files for NUV, optical, and NIR? Type which ones to use: nuv=0, opt=1, nir=2
-specs = [0,1,2]
+specs = [1]
 
 # 3) Do you want to use Vacuum wavelengths?
 vacuum = False
@@ -42,11 +42,14 @@ normalize = False
 # 5) Choose how many sigmas to clip from the continuum array
 sigmas_away = 3
 
-# 6) Do you want to see the plots of the fitted continuum?
-plot = False
+# 6) What is the width of the window to use to find local continuum?
+window = 350
 
-# 7) write the text file with the line net fluxes and equivalent widths?
-text_table = True
+# 7) Do you want to see the plots of the fitted continuum?
+plot = True
+
+# 8) write the text file with the line net fluxes and equivalent widths?
+text_table = False
 
 
 ############################################################################################################################################
@@ -73,13 +76,13 @@ order = None
 for d, s in zip(data, specs):
     print 'Working with:  %s' % full_file_list[s]
     # Correct spectra for redshift and calculate continuum with a polynomial of nth order
-    object_spectra, fitted_continuum = spectrum.fit_continuum(object_name, d, z, order=order, sigmas_away=sigmas_away, window=350, plot=plot, z_correct=True, normalize=normalize)
+    object_spectra, fitted_continuum, err_cont_fit = spectrum.fit_continuum(object_name, d, z, order=order, sigmas_away=sigmas_away, window=window, plot=plot, z_correct=True, normalize=normalize)
 
     # Obtain the lines net fluxes and EWs
     new_file_name = object_name+"_lineinfo"+spectrum_region[s]+".txt"
     lineinfo_text_file = os.path.join(results4object_path, new_file_name)
     
-    object_lines_info = spectrum.find_lines_info(object_spectra, fitted_continuum, lineinfo_text_file, text_table=text_table, vacuum=vacuum)
+    object_lines_info = spectrum.find_lines_info(object_spectra, fitted_continuum, err_cont_fit, lineinfo_text_file, text_table=text_table, vacuum=vacuum)
     print ''
 print sigmas_away, 'sigmas_away'    
 print 'Code finished!'
