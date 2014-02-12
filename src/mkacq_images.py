7aa0b11d7072ef92b8dd5e911e053bdc1781d734
Maria Peña-Guerrero <pena@stsci.edu>
2014-02-11 22:18:57 -0500

    position angle for the slit fixed

--- src/mkacq_images.py
+++ src/mkacq_images.py
@@ -22,9 +22,11 @@ objects_list =['iiizw107', 'iras08339', 'mrk1087', 'mrk1199', 'mrk5', 'mrk960', 
 #                 9           10          11       12          13       14       15         16         17
 
 ''' Choose parameters to run script'''
-object_number = 0
-vmin = 18
-vmax = 65
+object_number = 11
+vmin = 10
+vmax = 60
+#vmin = 10
+#vmax = 90
 
 # want to save images?  (type 'y' for yes or 'n' for no)
 save_plt = 'n'
@@ -36,7 +38,7 @@ change_format = True
 new_filetype = 'eps'
 
 # Do you want to see the plots separate or together? (choose separate=True for creating the jpg images)
-separate = False
+separate = True
 
 # Do you want to see the rotation angle of the slit in the 2d spectra?
 rotate = False
@@ -54,6 +56,11 @@ img_list = ['obqn18wbq_raw.fits', 'obqn08a6q_raw.fits', 'obqn03ogq_raw.fits', 'o
             'obqn10c4q_raw.fits', 'obqn07pcq_raw.fits', 'obqn16plq_raw.fits']
 img_name = img_list[object_number]
 
+full_names_list = ['IIIZw 107', 'IRAS 08339+6517', 'MRK 1087', 'MRK 1199', 'MRK 5', 'MRK 960', 'NGC 1741', 'POX 4', 'SBS 0218+003',
+               'SBS 0948+532', 'SBS 0926+606', 'SBS 1054+365', 'SBS 1319+579', 'TOL 1457-262', 'TOL 9', 'ARP 252', 'IRAS 08208+2816',
+               'SBS 1415+437']
+full_name = full_names_list[object_number]
+
 #fits_path = '/Users/pena/Documents/STIS/prop12472/raw_data'
 fits_path = '../HSTdata/raw_acq_imgs'
 jpgs_path = '../results/plots/object_images'
@@ -70,15 +77,33 @@ PA = ORIENT-45.  # the 45.35 came from the STIS data Handbook for a 52x0.2 slit
 print 'PA = ', PA
 
 
-def add_slit_line(lolim, uplim):
+def add_slit_line(lolim, uplim, teta):
+    if (teta <= 90.0) and (teta >= -90.0):
+        alfa = -1*(teta) * numpy.pi / 180.0
+        print 'quadrant 1 or 2'
+    elif (teta > 90.0) and (teta < 180.0):
+        alfa = (180.0 - teta) * numpy.pi / 180.0
+        print 'quadrant 3'
+    elif (teta > 180.0) and (teta < 270.0):
+        alfa = (-1)*(180.0 - teta) * numpy.pi / 180.0
+        print 'quadrant 4'
+    elif (teta < -90.0) and (teta > -180.):
+        alfa = (180.0 - teta) * numpy.pi / 180.0
+        print 'quadrant 4'
+    elif (teta < -180.0) and (teta > -270.):
+        alfa = ((-1)*teta - 180.0) * numpy.pi / 180.0
+        print 'quadrant 3'
     x = [uplim/2, uplim/2]
     y = [lolim, uplim]
-    teta = 180.0 - numpy.fabs(PA)
-    co = numpy.fabs((uplim/2.0 / numpy.cos(teta)) * numpy.sin(teta))
+    ca = uplim/2.0
+    co = (ca / numpy.cos(alfa)) * numpy.sin(alfa)
     z0 = x[0] - co
     z1 = x[0] + co
     z = [z0, z1]
-    print 'teta =', numpy.cos(teta)*(180/numpy.pi)
+    # draw plane
+    #plt.plot(x, y, 'k-', lw=2)
+    #plt.plot(y, x, 'k-', lw=2)
+    # now draw slit
     plt.plot(z, y, 'w-', lw=2)
     plt.xlim(lolim, uplim)
     plt.ylim(lolim, uplim)
@@ -88,6 +113,9 @@ jpg_img_name = object_name+'_before'+'.jpg'
 destination = os.path.join(jpgs_path, jpg_img_name)
 jpg_img_name_after = object_name+'_after'+'.jpg'
 destination_after = os.path.join(jpgs_path, jpg_img_name_after)
+lolim = 0
+uplim = 95    
+pangle = round(PA, 2)
 if separate:
     # before
     f1 = plt.figure(1, figsize=(10, 10))
@@ -111,8 +139,15 @@ if separate:
         im2 = plt.imshow(rotate_im2, vmin=vmin, vmax=vmax, origin='lower', cmap=cmap)
     else:
         im2 = plt.imshow(img_recenter, vmin=vmin, vmax=vmax, origin='lower', cmap=cmap)
-    plt.title(object_name+'_after')
-    plt.colorbar(im2, orientation='vertical')
+    #plt.title(object_name+'_after')
+    #plt.title(object_name+'_PA'+repr(pangle))
+    color = 'w'
+    size = 'x-large'
+    plt.text(60, 10, full_name, size=size, color=color)
+    plt.text(60, 6, 'P.A. = '+repr(pangle), size=size, color=color)
+    add_slit_line(lolim, uplim, PA)
+    cbar_ax = f2.add_axes([0.92, 0.11, 0.03, 0.776])
+    plt.colorbar(im2, cax=cbar_ax)#, orientation='vertical')
     if save_plt == 'y':
         plt.savefig(destination_after)
         print('Plot %s was saved!' % destination_after)
@@ -133,10 +168,8 @@ else:
         im2 = ax2.imshow(img_recenter, vmin=vmin, vmax=vmax, origin='lower', cmap=cmap)
     ax1.set_title(object_name+'_before')
     # After
-    lolim = 0
-    uplim = 90    
-    add_slit_line(lolim, uplim)
-    ax2.set_title(object_name+'_after')
+    add_slit_line(lolim, uplim, PA)
+    ax2.set_title(object_name+'_after_recenter'+'_PA'+repr(pangle))
     cbar_ax = f.add_axes([0.92, 0.15, 0.02, 0.7])
     f.colorbar(im1, cax=cbar_ax)
     plt.show()
