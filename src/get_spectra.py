5fb141e495df11aec85f7bc7ef0d497c6129948a
Maria Peña-Guerrero <penaguerrero.m@gmail.com>
2014-02-21 00:04:21 -0500

    major changes. Code is ready to make nice spectra

--- src/get_spectra.py
+++ src/get_spectra.py
@@ -6,31 +6,37 @@ from PIL import Image
 '''
 This code uses the 1d fits extraction files from all three filters (g230l, g430l, and g750l), 
 in order to let me choose which one has the best S/N. 
+'''
+
+#####################################################################################################################################
+
+# short name of the object
+#                 0           1           2            3         4        5          6        7         8
+objects_list =['iiizw107', 'iras08339', 'mrk1087', 'mrk1199', 'mrk5', 'mrk960', 'ngc1741', 'pox4', 'sbs0218',
+               'sbs0948', 'sbs0926', 'sbs1054', 'sbs1319', 'tol1457', 'tol9', 'arp252', 'iras08208', 'sbs1415']
+#                 9           10         11         12         13       14        15         16         17
+
+''' CHOOSE THE OBJECT NUMBER '''
+object_number = 0
+
+#####################################################################################################################################
 
 # reference IDs for sample objects:
-0. iiizw107 = 180              9. sbs0948 = 110
-1. iras08339 = 080            10. sbs0926 = 190
-2. mrk1087 = 030              11. sbs1054 = 130
-3. mrk1199 = 060              12. sbs1319 = 150
-4. mrk5 = 050                 13. tol1457 = 170
-5. mrk960 = 010               14. tol9 = 120
-6. ngc1741 = 040              15. arp252 = 100
-7. pox4 = 140                 16. iras08208 = 070
-8. sbs0218 = 020              17. sbs1415 = 160
-'''
+obqn_num_list = ['180', '080', '030', '060', '050', '010', '040', '140', '020', 
+                 '110', '190', '130', '150', '170', '120', '100', '070', '160']
+
+full_names_list = ['IIIZw 107', 'IRAS 08339+6517', 'MRK 1087', 'MRK 1199', 'MRK 5', 'MRK 960', 
+                   'NGC 1741', 'POX 4', 'SBS 0218+003', 'SBS 0948+532', 'SBS 0926+606', 'SBS 1054+365', 
+                   'SBS 1319+579', 'TOL 1457-262', 'TOL 9', 'ARP 252', 'IRAS 08208+2816', 'SBS 1415+437']
+
+obj_name = objects_list[object_number]
+obqn_num = obqn_num_list[object_number]
+full_name = full_names_list[object_number]
 
 path_oneDspecs = '../HSTdata/'
 path_oneDspecs_notMain = '../HSTdata/not_main_specs/'
 path_results = '../results/1Dspecs/'
 path_plots = '../results/plots/'
-
-print('Please type first 3 digits of obqn number for the object of interest (i.e. obqn18010_... type 180)')
-print('    OR ')
-print('type the full path to a .txt file containing the HST obs number (obqn number) and the name of the object separated by a coma.')
-print('      i.e.  180, iiizw107')
-print('            030, mrk1087')
-
-obqn_num = raw_input()
 
 # In case of wanting to study the non main spectra, change to True
 main_specs = True
@@ -41,15 +47,13 @@ if main_specs == False:
 # This is assuming that the fits files have been cleaned from cosmic rays with the cosmic_rays script
 oneDspecs = glob.glob(path_oneDspecs+'obqn'+obqn_num+'*x*.fits')    
 
-print('Please type name of object or some identifier (i.e. ngc6822)')
-obj_name = raw_input()
 # Create the name for the plot
 selectedspecs_file = obj_name+'_selectedspecs'
 txtf = path_results+selectedspecs_file
 img_format = '.jpg'
 plot_name = os.path.join(path_plots, obj_name)
 # plot the spectra for the input object
-used_specs = metallicity.OneDspecs(oneDspecs, txtf, plot_name, img_format)
+used_specs = metallicity.OneDspecs(oneDspecs, txtf, plot_name, img_format, full_name)
 
 print("I'm done and I live.")
 
