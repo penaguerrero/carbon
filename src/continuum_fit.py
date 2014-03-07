ec24b80ba4999fafaa12fc822f25d87d26b071f7
Maria Peña-Guerrero <pena@stsci.edu>
2014-03-07 00:50:54 -0500

    minor modifications

--- src/continuum_fit.py
+++ src/continuum_fit.py
@@ -1,4 +1,4 @@
-#import numpy
+import numpy
 import os
 from science import spectrum
 
@@ -17,10 +17,10 @@ objects_list =['iiizw107', 'iras08339', 'mrk1087', 'mrk1199', 'mrk5', 'mrk960', 
 # Choose parameters to run script
 
 # 1) Select a number from objects_list, i = :
-object_number = 5
+object_number = 17
 
 # 2) use all 3 files for NUV, optical, and NIR? Type which ones to use: nuv=0, opt=1, nir=2
-specs = [0]
+specs = [2]
 
 # 3) Do you want to use Vacuum wavelengths?
 vacuum = False
@@ -32,16 +32,16 @@ normalize = False
 sigmas_away = 3
 
 # in case I want to use a specific order for the polynomial, else it will be determined by the algorithm
-order = 2
+order = 1
 
 # 6) What is the width of the window to use to find local continuum?
-window = 500
+window = 550
 
 # 7) Do you want to see the plots of the fitted continuum?
 plot = True
 
 # 8) write the text file with the line wavelengths, fluxes, and fitted continuum?
-text_table = True
+text_table = False
 
 # Want to see the quiasi-final spectrum?  (i.e. correct for redshift and rebin)
 correct_redshift = False
@@ -70,21 +70,21 @@ data, full_file_list = spectrum.loadtxt_from_files(object_name, add_str, specs, 
 #altern = '../results/sbs1319/sbs1319_opt_corr1.txt'
 #f, w = numpy.loadtxt(altern, skiprows=5, usecols=(1,2), unpack=True)
 ### To get altern files run: 1. correct_spec script, 2.rspectext, 3.splot, 4.correct with j and save with i, 5.wspectext
-#altern = '../results/tol9/tol9_opt_corr1.txt'
+#altern = '../results/tol9/tol9_opt21_fix.txt'
 #w, f = numpy.loadtxt(altern, unpack=True)
 #data = [numpy.array([w,f])]
 
 # Terminations used for the lines text files
 spectrum_region = ["_nuv", "_opt", "_nir"]
 
-z_list = [0.01985, 0.019581, 0.02813, 0.01354, 0.002695, 0.024076, 0.01348, 0.01201, 0.05842,
-          0.046240, 0.013642, 0.002010, 0.0076, 0.01763, 0.010641, 0.032989, 0.04678, 0.002031]
-# original phase 2 z for iras08339 0.019113, mrk960 0.021371
+z_list = [0.01985, 0.019581, 0.02813, 0.01354, 0.002695, 0.02346, 0.013631, 0.01201, 0.05842,
+          0.046240, 0.013642, 0.002010, 0.0076, 0.01732, 0.01199, 0.032989, 0.04678, 0.002031]
+# original phase 2 z for iras08339 0.019113, mrk960 0.021371, ngc1741 0.013631, tol1457 0.01763, tol9 0.010641
 
 '''The STIS data handbook gives a dispersion of 0.60, 1.58, 2.73, and 4.92 Angstroms per pixel for grating settings G140L, 
 G230L, G430L, and G750L, respectively. The resolution element is ~1.5 pixels. '''
 originals = [1.58, 2.73, 4.92]
-desired_disp_list = [2.5, 8.0, 10.0]
+desired_disp_list = [2.0, 8.0, 6.0]
 
 for d, s in zip(data, specs):
     print 'Working with:  %s' % full_file_list[s]
@@ -106,7 +106,10 @@ for d, s in zip(data, specs):
 
     if text_table == True:
         # Write text file of wavelengths, fluxes, and fitted continuum
-        new_file_name = object_name+spectrum_region[s]+".txt"
+        if normalize == False:
+            new_file_name = object_name+spectrum_region[s]+".txt"
+        else:
+            new_file_name = object_name+spectrum_region[s]+"_normalized.txt"
         file_name = os.path.join(results4object_path, new_file_name)
         txt = open(file_name, 'w+')
         print >> txt, '{:<10} {:>30} {:>35}'.format('Wavelength [A]', 'Flux [ergs/s/cm$^2$/$\AA$]', 'Continuum [ergs/s/cm$^2$/$\AA$]')
@@ -117,7 +120,7 @@ for d, s in zip(data, specs):
     ### If needing to create a text file with only wavelengths and fluxes for splot change splot_text to true and change the corresponding part
     # of the spectra to appear in the title of the text file
     splot_text = False
-    part_of_spec = 'opt'
+    part_of_spec = 'nir'
     if splot_text == True:
         name_out_file = os.path.join(results4object_path, object_name+"_"+part_of_spec+"spec.txt")
         fout = open(name_out_file, 'w+')
