1ab6f198b42eef37cbb1be1a30c15a980aa3c972
Maria Peña-Guerrero <pena@stsci.edu>
2014-05-15 17:25:07 -0400

    minor changes

--- src/continuum_fit.py
+++ src/continuum_fit.py
@@ -17,10 +17,10 @@ objects_list =['iiizw107', 'iras08339', 'mrk1087', 'mrk1199', 'mrk5', 'mrk960', 
 # Choose parameters to run script
 
 # 1) Select a number from objects_list, i = :
-object_number = 12
+object_number = 16
 
 # 2) use all 3 files for NUV, optical, and NIR? Type which ones to use: nuv=0, opt=1, nir=2
-specs = [2]
+specs = [0]
 
 # 3) Do you want to use Vacuum wavelengths?
 vacuum = False
@@ -29,19 +29,19 @@ vacuum = False
 normalize = False
 
 # 5) Choose how many sigmas to clip from the continuum array
-sigmas_away = 2
+sigmas_away = 3
 
 # in case I want to use a specific order for the polynomial, else it will be determined by the algorithm
-order = 1
+order = 7
 
 # 6) What is the width of the window to use to find local continuum?
-window = 550
+window = 50
 
 # 7) Do you want to see the plots of the fitted continuum?
 plot = True
 
 # 8) write the text file with the line wavelengths, fluxes, and fitted continuum?
-text_table = False
+text_table = True
 
 # Want to see the quasi-final spectrum?  (i.e. correct for redshift and rebin)
 correct_redshift = False
