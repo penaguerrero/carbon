90a83b21cd85fb29fde0375ddc1b69e0bea77148
Maria Peña-Guerrero <pena@stsci.edu>
2014-08-04 13:37:59 -0400

    minor changes

--- src/full_analysis.py
+++ src/full_analysis.py
@@ -11,7 +11,7 @@ from science import spectrum
 '''  Choose parameters to run script  '''
 
 # name of the object
-#                 0             1         2            3         4        5*        6         7         8
+#                  0            1           2          3         4        5*        6         7         8
 objects_list =['iiizw107', 'iras08339', 'mrk1087', 'mrk1199', 'mrk5', 'mrk960', 'ngc1741', 'pox4', 'sbs0218',
                'sbs0948', 'sbs0926', 'sbs1054', 'sbs1319', 'tol1457', 'tol9', 'arp252', 'iras08208', 'sbs1415']
 #                 9           10         11         12         13       14        15         16         17
@@ -34,6 +34,13 @@ case = 'B'
 
 ############################################################################################################################################
 
+# Skip the finding of the line info and go directly to gather the spectra?
+#                            0     1     2     3     4     5     6     7     8
+use_given_lineinfo_list = [True, True, True, False, True, False, True, True, True, 
+                           True, True, True, True, True, True, True, True, True]
+#                            9     10    11    12    13    14    15    16    17
+use_given_lineinfo = use_given_lineinfo_list[object_number]
+
 # In case of wanting to use a specific temperature and/or density (accepts values with errors as lists)
 #                      0                1                 2                3          4 (based on Na4)     5*    6     7     8           
 forceTe_list = [[10900.,15000.],  [9000., 16000.], [10500.,12500.], [10100.,12100.], [15000.0, 17000.0], None, None, None, 11900.0,
@@ -43,16 +50,9 @@ forceTe = forceTe_list[object_number]
 #forceTe = None
 #                   0      1             2              3        4     5    6     7     8           
 forceNe_list = [None, [100., 650.], [100., 3000.], [800, 1000], None, None, None, None, [100., 655],
-                None,    None, None, None, None, None, None, None, [500., 400.]]
+                None,    None, None, None, None, None, None, None, [100, 200.]]
 #                 9      10*     11    12    13    14    15    16    17 
 forceNe = forceNe_list[object_number]
-
-# Skip the finding of the line info and go directly to gather the spectra?
-#                            0     1     2     3     4      5     6     7     8
-use_given_lineinfo_list = [True, True, True, False, True, False, True, True, True, 
-                           False, False, False, True, False, False, False, False, True]
-#                           9     10     11     12     13     14     15     16     17
-use_given_lineinfo = use_given_lineinfo_list[object_number]
 
 # use this option if wanting to rebin or not
 #                       0      1      2     3      4      5     6     7     8
