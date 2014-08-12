5de2d4f46c749dc78299297fac1298842ff7ccbb
Maria Peña-Guerrero <penaguerrero.m@gmail.com>
2014-08-12 13:38:31 -0400

    minor changes

--- src/comparisonWithLopSan09.py
+++ src/comparisonWithLopSan09.py
@@ -4,7 +4,8 @@ import os
 
 ############################################################################################################################################
 '''
-This scripts takes the found metallicities with my measurements and those by the code and produces:
+This scripts finds the difference between our adopted metallicity values and those presented in Lopez-Sanchez & Esteban (2009).
+The results are:
  * table of comparison values of all available abundances
 '''
 
@@ -21,4 +22,32 @@ O, Oerr, Oother, Oerrother, N, Nerr, Nother, Nerrother, Ne, Neerr, Neother, Neer
 #                objects in the sample 
 objects_list =['iiizw107', 'iras08339', 'mrk1087', 'mrk1199', 'mrk5', 'mrk960', 'ngc1741', 'pox4', 'sbs0218',
                'sbs0948', 'sbs0926', 'sbs1054', 'sbs1319', 'tol1457', 'tol9', 'arp252', 'iras08208', 'sbs1415']
+
+default_cols = ['Object', 'log_O/H', 'err', 'log_N/H', 'err', 'log_Ne/H', 'err', 'log_S/H', 'err', 'log_Ar/H', 'err', 'log_Fe/H', 'err', 'Te_[OIII]', 'err']
+
+diff_O = []
+diff_N = []
+diff_Ne = []
+diff_S = []
+diff_Ar = []
+diff_Fe = []
+diff_T = []
+
+for ox, oo, ni, no, ne, neo, su, so, ar, aro, fe, feo, Te, Teo in zip(O, Oother, N, Nother, Ne, Neother, S, Sother, Ar, Arother, Fe, Feother, T, Tother):
+    diff = ox - oo
+    diff_O.append(diff)
+    diff = ni - no
+    diff_N.append(diff)
+    diff = ne - neo
+    diff_Ne.append(diff)
+    diff = su - so
+    diff_S.append(diff)
+    diff = ar - aro
+    diff_Ar.append(diff)
+    diff = fe - feo
+    diff_Fe.append(diff)
+    diff = Te - Teo
+    diff_T.append(diff)
+
+
 
