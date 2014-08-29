b2ca4271b6a945a3da48a6e4e2f8186cfd2e1dd8
Maria Peña-Guerrero <pena@stsci.edu>
2014-08-29 16:55:48 -0400

    minor changes

--- src/full_analysis.py
+++ src/full_analysis.py
@@ -15,7 +15,7 @@ from science import spectrum
 objects_list =['iiizw107', 'iras08339', 'mrk1087', 'mrk1199', 'mrk5', 'mrk960', 'ngc1741', 'pox4', 'sbs0218',
                'sbs0948', 'sbs0926', 'sbs1054', 'sbs1319', 'tol1457', 'tol9', 'arp252', 'iras08208', 'sbs1415']
 #                 9           10         11         12         13       14        15         16         17
-object_number = 5
+object_number = 6
 # Is this a TEST run?
 TEST_run = True
 
