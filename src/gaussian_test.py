4edea568aed8aa82b9cd647b2630ab4697c7f8c0
Maria Peña-Guerrero <pena@stsci.edu>
2015-04-14 16:31:34 -0400

    cleaned code

--- src/gaussian_test.py
+++ src/gaussian_test.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 from scipy import optimize
 from scipy import stats
 
-region = 2
+region = 1
 
 if region == 1:
     region = 'nuv'
@@ -34,14 +34,32 @@ def fit_gauss(x, y):
     return gf, sig
 
 def multi_gauss(x, *args):
-    m1, m2, s1, s2, a1, a2 = args
-    ret = gauss(x, a1, m1, s1)#a1 * stats.norm.pdf(x, loc=m1, scale=s1)
+    '''
+    This function fits 2 or 3 gaussians to the given array of wavelength.
+    '''
+    l = len(args)
+    if l == 6:
+        m1, m2, s1, s2, a1, a2 = args
+    elif l == 9:
+        m1, m2, m3, s1, s2, s3, a1, a2, a3 = args
+    ret = gauss(x, a1, m1, s1)
     ret += gauss(x, a2, m2, s2)
+    if l == 9:
+        ret += gauss(x, a3, m3, s3)
     return ret
 
-def fit_multi_gauss(x, y, m1, m2):
-    s1, s2, a1, a2 = 1.2, 1.2, 3.0e-15, 3.0e-15
+def fit_multi_gauss(x, y, m1, m2, m3=None):
+    '''
+    This function finds the best fits 2 or 3 gaussians to the given array of wavelength.
+    '''
+    #s1, s2, s3, a1, a2, a3 = 1.2, 1.2, 1.2, 3.0e-15, 3.0e-15, 3.0e-15
+    s1, s2, s3, a1, a2, a3 = 1.0, 1.0, 1.0, 3.0e-16, 3.0e-16, 3.0e-16
+    m1, m2 = float(m1), float(m2)
     p0 = [m1, m2, s1, s2, a1, a2]
+    if m3 != None:
+        m3 = float(m3)
+        print m1, m2, m3
+        p0 = [m1, m2, m3, s1, s2, s3, a1, a2, a3]
     coeffs, _ = optimize.curve_fit(multi_gauss, x, y, p0=p0)
     mg = multi_gauss(x, *coeffs)
     print 'these are the coefficients:  ', coeffs
@@ -55,8 +73,8 @@ wav, flx = np.loadtxt(spec, unpack=True)
 #data_arr = [wav, flx]
 cont_arr0 = spectrum.theo_cont(wav, scale_factor=0.0)   # this is an array of wavelength and continuum
 
-lolim = 3710.0#4990.0
-uplim = 3740.0#5024.5
+lolim = 1895.0#3710.0#4990.0
+uplim = 1915.0#3740.0#5024.5
 linew = wav[(wav>=lolim) & (wav<=uplim)]
 linef = flx[(wav>=lolim) & (wav<=uplim)]
 data_arr = [linew, linef]
@@ -68,16 +86,17 @@ wavelength, flux, _, flux_cont = spectrum.fill_EWarr(data_arr, cont_arr, lolim, 
 gf, sig = fit_gauss(wavelength, flux)
 fwhm = spectrum.FWHM(sig)
 
-#meanX = 3726.30454854
-mean1 = 3722.0
-mean2 = 3729.0
-norm1 = 3.0e-15
-norm2 = 3.0e-15
-sig1 = 2.2
-sig2 = 2.2
-gsum, coeffs = fit_multi_gauss(wavelength, flux, mean1, mean2)
-g1 = gauss(wavelength, coeffs[4], coeffs[0], coeffs[2])
-g2 = gauss(wavelength, coeffs[5], coeffs[1], coeffs[3])
+mean1 = 1901#3726
+mean2 = 1907#3729
+mean3 = 1909#3721
+
+gsum, coeffs = fit_multi_gauss(wavelength, flux, mean1, mean2, mean3)
+#             x           a         mean        sig
+#g1 = gauss(wavelength, coeffs[4], coeffs[0], coeffs[2])
+#g2 = gauss(wavelength, coeffs[5], coeffs[1], coeffs[3])
+g1 = gauss(wavelength, coeffs[6], coeffs[0], coeffs[3])
+g2 = gauss(wavelength, coeffs[7], coeffs[1], coeffs[4])
+g3 = gauss(wavelength, coeffs[8], coeffs[2], coeffs[5])
 
 plt.xlabel('Wavelength  [$\AA$]')
 plt.ylabel('Flux  [ergs/s/cm$^2$/$\AA$]')
@@ -85,9 +104,10 @@ plt.xlim(lolim-100.0, uplim+100.0)
 plt.plot(wav, flx, 'k')
 plt.plot(wavelength, flux, 'b')
 plt.plot(wavelength, gf, 'r')
-plt.plot(wavelength, gsum, 'y--')
+plt.plot(wavelength, gsum, 'r--')
 plt.plot(wavelength, g1, 'c--')
 plt.plot(wavelength, g2, 'c--')
+plt.plot(wavelength, g3, 'c--')
 plt.plot(cont_arr0[0], cont_arr0[1], 'g')
 plt.show()
 
