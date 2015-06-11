34395242f698e1e407320374f903d38c82ba9efe
Maria Peña-Guerrero <pena@stsci.edu>
2015-06-11 17:13:34 -0400

    program to create distribution function of grades

---
+++ src/agn_finalgrades.py
@@ -0,0 +1,36 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import matplotlib.mlab as mlab
+import math
+
+fname = 'agn_day1.txt'
+
+propnum, means, devs = np.loadtxt(fname, skiprows=2, usecols=(0,11,12), unpack=True)
+
+props = np.array([])
+for i, p in enumerate(propnum):
+    props = np.append(props, i)
+
+MEAN = sum(means)/len(means)
+var = (means - MEAN)**2
+stdev = np.sqrt( 1.0/(len(means)-1.0) * sum(var))
+print 'mean =', MEAN, '    standard dev =', stdev
+
+stmeans = (means - MEAN)/stdev
+#print stmeans
+
+m = 0
+variance = 1
+sigma = math.sqrt(variance)
+x = np.linspace(-3,3,100)
+height = len(means)*100.0 / 40.0
+y = mlab.normpdf(x,m,sigma) * height
+plt.plot(x,y)
+
+plt.title('Partial Final Grading ($\mu$=2.97)') 
+plt.xlabel('$\sigma$ [1 $\sigma$=0.68]')
+plt.ylabel('Proposals graded')
+plt.plot(stmeans, props, 'ko')
+fig = 'partial_final_grading.jpg'
+plt.savefig(fig)
+plt.show()
