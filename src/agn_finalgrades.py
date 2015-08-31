bab82fbc03f9143099c124893fc01f8082d7ddc7
Maria Pena-Guerrero <pena@stsci.edu>
2015-08-31 15:16:47 -0400

    minor changes

--- src/agn_finalgrades.py
+++ src/agn_finalgrades.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import pylab 
 import matplotlib.mlab as mlab
 import math
 
@@ -19,18 +20,30 @@ print 'mean =', MEAN, '    standard dev =', stdev
 stmeans = (means - MEAN)/stdev
 #print stmeans
 
+#histo = np.histogram(stmeans, bins=5, range=(-3.,3.))
+#print histo
+#plt.plot(histo[0])
+
+# the histogram od the data
+n, bins, patches = pylab.hist(stmeans, bins=10, histtype='stepfilled')
+pylab.setp(patches, 'facecolor', 'b', 'alpha', 0.75)
+
+# add a line showing the expected distribution
+yh = pylab.normpdf(bins, 0.0, stdev)
+lh = pylab.plot(bins, yh, 'k--', linewidth=1.5)
+
 m = 0
 variance = 1
 sigma = math.sqrt(variance)
 x = np.linspace(-3,3,100)
 height = len(means)*100.0 / 40.0
 y = mlab.normpdf(x,m,sigma) * height
-plt.plot(x,y)
+#plt.plot(x,y)
 
 plt.title('Partial Final Grading ($\mu$=2.97)') 
 plt.xlabel('$\sigma$ [1 $\sigma$=0.68]')
 plt.ylabel('Proposals graded')
-plt.plot(stmeans, props, 'ko')
+#plt.plot(stmeans, props, 'ko')
 fig = 'partial_final_grading.jpg'
-plt.savefig(fig)
+#plt.savefig(fig)
 plt.show()
