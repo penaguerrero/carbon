e451a9a167f3dae1a60c787d58ffbd04198a6eb7
Maria Pena-Guerrero <pena@stsci.edu>
2016-06-08 14:10:24 -0400

    improved code with CHbeta reading capability

--- src/bpt_diag.py
+++ src/bpt_diag.py
@@ -1,6 +1,7 @@
 import numpy as np
 import string
 import os
+import matplotlib
 import matplotlib.pyplot as plt
 
 '''
@@ -12,7 +13,7 @@ This script creates a BPT plot using our line intensities AND those from Lopez-S
 
 save_plot = False
 img_type = 'jpg'
-show_LopSan_data = False
+show_LopSan_data = True
 sampleBPTtxt = False
 C_Hbeta = True
 
@@ -42,9 +43,15 @@ full_names_list = ['IIIZw 107', 'IRAS 08339+6517', 'MRK 1087', 'MRK 1199', 'MRK 
                'SBS 1415+437']
 
 # divide the sample in objects for in which we found Te and those in which we did not
-objects_with_Te = ['mrk5', 'pox4', 'sbs0948', 'sbs1054', 'tol1457', 'iras08208']
-objects_Te_literature = ['iiizw107', 'iras08339', 'mrk1087', 'mrk1199', 'mrk960', 'ngc1741', 'sbs0218',
-                         'sbs0926', 'sbs1319', 'tol9', 'arp252', 'sbs1415']
+# BEFORE
+#objects_with_Te = ['mrk5', 'pox4', 'sbs0948', 'sbs1054', 'tol1457', 'iras08208']
+#objects_Te_literature = ['iiizw107', 'iras08339', 'mrk1087', 'mrk1199', 'mrk960', 'ngc1741', 'sbs0218',
+#                         'sbs0926', 'sbs1319', 'tol9', 'arp252', 'sbs1415']
+# NOW
+# now divide the sample in objects for in which we found Te and those in which we did not
+objects_with_Te = ['iiizw107', 'mrk5', 'pox4', 'sbs0948', 'sbs1054', 'tol1457', 'iras08208', 'mrk1087', 
+                   'mrk1199', 'mrk960', 'ngc1741', 'sbs0218', 'sbs0926', 'sbs1319', 'arp252', 'sbs1415']
+objects_Te_literature = ['iras08339', 'tol9']
 
 # File containing the SDSS outlines
 bpt_contours = '../cloudy_tests/pycloudy/BPT_TEST/bpt_contours.txt'
@@ -116,6 +123,14 @@ for object_number, object_name in enumerate(objects_list):
     I4861err = np.append(I4861err, meas_Ierr[0])
     I5007err = np.append(I5007err, meas_Ierr[1])
     I6563err = np.append(I6563err, meas_Ierr[2])
+    if object_name=='sbs1319':
+        print I5007err[object_number], I5007err[object_number]/I5007[object_number]*100.
+        I5007err[object_number] = I5007[object_number]*0.2
+        print I5007err[object_number]
+    if object_name=='iras08208':
+        print I5007err[object_number], I5007err[object_number]/I5007[object_number]*100.
+        I5007err[object_number] = I5007[object_number]*0.15
+        print I5007err[object_number]
 #print 'I5007err', I5007err
 #print 'I6563err', I6563err
 
@@ -143,6 +158,10 @@ if sampleBPTtxt:
 
 
 # PLOT
+font = {#'family' : 'normal',
+        'weight' : 'normal',
+        'size'   : 16}
+matplotlib.rc('font', **font)
 
 fig = plt.figure(1, figsize=(12, 10))
 plt.xlim(-2.3, 0.5)
@@ -151,12 +170,16 @@ xlab = r'log ([NII]6584/H$\alpha$)'
 ylab = r'log ([OIII]5007/H$\beta$)'
 plt.xlabel(xlab)
 plt.ylabel(ylab)
+plt.minorticks_on()
 #  Line intensities from Lopez-Sanchez et al.
 #plt.title('Data from Lopez-Sanchez et al.')
 x = np.log10(lop6584/lop6563)
 y = np.log10(lop5007/100.0)
 if show_LopSan_data:
-    plt.plot(x, y, 'ro')
+    #plt.plot(x, y, 'ro')
+    xerr = 0.434*(lop5007err/lop5007)
+    yerr = 0.434*(lop6584err/lop6584)
+    plt.errorbar(x, y, xerr=xerr, yerr=yerr, fmt='rd', ecolor='r', markersize=8)
 # STIS data
 x0 = np.log10(lop6584/I6563)
 y0 = np.log10(I5007/100.0)
@@ -165,20 +188,52 @@ e1 = lop6584/I6563 * np.sqrt( (lop6584err/lop6584)**2 + (I6563err/I6563)**2 )
 x0err = e1 / (2.303 * (lop6584/I6563) )
 e1 = I5007err/100.0
 y0err = e1 / (2.303 * (I5007/100.0) )
+
 # SDSS data
 plt.plot(logNHa, medOHa, 'b')
-plt.plot(logNHa, medOHaminus3sig, 'b--')
-plt.plot(logNHa, medOHaplus3sig, 'b--')
+plt.plot(logNHa, medOHaminus3sig, 'b', alpha=0.4)
+plt.plot(logNHa, medOHaplus3sig, 'b', alpha=0.4)
+
+# Kewley 2001 theoretical star-forming upper limits
+value_n2ha = -2.4
+N2Ha = [] 
+for _ in range(28):
+    value_n2ha += 0.1
+    N2Ha.append(value_n2ha)
+N2Ha = np.array([N2Ha])
+O3Hb =  0.61 / (N2Ha-0.47) + 1.19
+plt.plot(N2Ha[0], O3Hb[0], 'g:')
+
+# Kauffman 2003 AGN and LINER lower limit
+O3Hbagn =  0.61 / (N2Ha-0.05) + 1.3   # AGN
+plt.plot(N2Ha[0][:-5], O3Hbagn[0][:-5], 'm-.')
+value_n2ha = -0.5
+N2Ha = [] 
+for _ in range(28):
+    value_n2ha += 0.1
+    N2Ha.append(value_n2ha)
+N2Ha = np.array([N2Ha])
+O3Hbliner =  1.65*N2Ha + 0.245   # LINER
+plt.plot(N2Ha[0], O3Hbliner[0], 'm--')
+
+# Insert labels of type of object
+plt.annotate('H II', xy=(1.21, -0.21), xycoords='axes fraction', xytext=(0.25, 0.5))
+plt.annotate('AGN', xy=(1.21, -0.21), xycoords='axes fraction', xytext=(0.55, 0.85))
+plt.annotate('LINER', xy=(1.21, -0.21), xycoords='axes fraction', xytext=(0.85, 0.55))
+#plt.annotate('Comp', xy=(1.21, -0.21), xycoords='axes fraction', xytext=(0.78, 0.27))
+
 # Insert name of object in the plot
 i = 0
 for x0i, y0i, z in zip(x0, y0, objects_list):
     if z in objects_with_Te:
         fmt='ko'
+        markersize=7
     elif z in objects_Te_literature:
-        fmt='wo'
+        fmt='k*'
+        markersize=12
     #fmt = 'go'
     #plt.plot(x0i, y0i, fmt)   # without error bars
-    plt.errorbar(x0i, y0i, xerr=x0err[i], yerr=y0err[i], fmt=fmt, ecolor='k')
+    plt.errorbar(x0i, y0i, xerr=x0err[i], yerr=y0err[i], fmt=fmt, ecolor='k', markersize=markersize)
     # Annotate the points 5 _points_ above and to the left of the vertex
     subxcoord = -3
     subycoord = 5
@@ -194,24 +249,47 @@ for x0i, y0i, z in zip(x0, y0, objects_list):
         subycoord = -12
     if C_Hbeta:
         subxcoord = -3
-        subycoord = 5
+        subycoord = 6
         side = 'right'
+        if (z == 'pox4'):
+            subycoord = 4
+        if (z == 'sbs1054') or (z == 'sbs0926'):
+            subxcoord = -20
+            subycoord = 10
+            side = 'left'
         if (z == 'mrk960') or (z == 'iras08339') or (z == 'arp252') or (z == 'iras08208'):
             subxcoord = 5
             side = 'left'
-        if (z == 'tol1457') or (z == 'sbs0948') or (z == 'tol9'):
+        if (z == 'tol1457') or (z == 'sbs0948') or (z == 'tol9') or (z == 'mrk5') or (z == 'sbs1415'):
             subycoord = -12
-        if (z == 'sbs0926') or (z == 'iras08339'):
+        if (z == 'iras08339'):
             subxcoord = 5
             subycoord = -12
             side = 'left'
-    plt.annotate('{}'.format(z), xy=(x0i,y0i), xytext=(subxcoord, subycoord), ha=side, textcoords='offset points')
+    plt.annotate('{}'.format(z), xy=(x0i,y0i), xytext=(subxcoord, subycoord), ha=side, textcoords='offset points', size=13)
     # ONLY Lopez-Sanchez data
     if show_LopSan_data:
         subxcoord = 5
         subycoord = -10
         side = 'left'
-        plt.annotate('{}'.format(z), xy=(x[i],y[i]), xytext=(subxcoord, subycoord), ha=side, textcoords='offset points', color='r')
+        if (z == 'arp252') or (z == 'sbs1054'):
+            subycoord = -3
+        if (z == 'iras08339'):
+            subycoord = -14
+        if (z == 'mrk960') or (z == 'tol9') or (z == 'mrk1087') or (z == 'ngc1741') or (z == 'sbs1415') or (z == 'pox4'):
+            subxcoord = -3
+            subycoord = 4
+            side = 'right'
+        if (z == 'mrk1087') or (z == 'mrk5'):
+            subxcoord = -3
+            subycoord = 1
+            side = 'right'
+        if (z == 'tol1457'):
+            subxcoord = -3
+            subycoord = -12
+            side = 'right'
+        plt.annotate('{}'.format(z), xy=(x[i],y[i]), xytext=(subxcoord, subycoord), ha=side, textcoords='offset points', 
+                     color='r', size=13)
     i = i+1
 
 if save_plot:
